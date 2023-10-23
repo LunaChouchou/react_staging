5db5cea382b75cd84c03161dad1f2958380fe340
Luna Chouchou <liu2shu1@outlook.com>
2023-10-24 00:16:08 +0900

    删除P126之前（ReactRouter6加更以外）的代码

--- 03_src_TodoList案例/index.js
+++
@@ -1,8 +0,0 @@
-//引入react核心库
-import React from 'react'
-//引入ReactDOM
-import { createRoot } from 'react-dom/client';
-//引入App
-import App from './App'
-
-createRoot(document.getElementById('root')).render(<App/>)
