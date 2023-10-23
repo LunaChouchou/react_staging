ee68a00cb044f17d61ebc239871c6500d0bbc6d0
Luna Chouchou <liu2shu1@outlook.com>
2023-10-24 00:13:40 +0900

    src差分確認用_01_src_一级路由

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-// HashRouter也可
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
