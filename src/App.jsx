f636a12d0cacd37903670f7fda344ed46618d6be
Luna Chouchou <liu2shu1@outlook.com>
2023-10-26 07:31:13 +0900

    src差分確認用_04_src_useRoutes路由表

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { NavLink, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
+import { NavLink, useRoutes } from 'react-router-dom'
+import routes from './routes'
 
 export default function App() {
-    function computedClassName({isActive}){
-        return isActive ? 'list-group-item atguigu' : 'list-group-item'
-    }
+    const element = useRoutes(routes)
+    // const element = useRoutes([
+    //     {
+    //         path: '/about',
+    //         element: <About/>
+    //     },
+    //     {
+    //         path: '/home',
+    //         element: <Home/>
+    //     },
+    //     {
+    //         path: '/',
+    //         element: <Navigate to="/about"/>
+    //     }
+    // ])
     return (
         <div className="container">
             <div className="row">
@@ -20,19 +31,15 @@ export default function App() {
                         {/* <a className="list-group-item" href="./about.html">About</a>
                         <a className="list-group-item active" href="./home.html">Home</a> */}
                         {/* 路由链接 */}
-                        <NavLink className={computedClassName} to="/about">About</NavLink>
-                        <NavLink className={computedClassName} to="/home">Home</NavLink>
+                        <NavLink className="list-group-item" to="/about">About</NavLink>
+                        <NavLink className="list-group-item" to="/home">Home</NavLink>
                     </div>
                 </div>
                 <div className="col-xs-6">
                     <div className="panel">
                         <div className="panel-body">
                             {/* 注册路由 */}
-                            <Routes>
-                                <Route path="/about" element={<About />} />
-                                <Route path="/home" element={<Home />} />
-                                <Route path="/" element={<Navigate to="/about" />} />
-                            </Routes>
+                            {element}
                         </div>
                     </div>
                 </div>
