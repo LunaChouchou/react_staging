cb5a170a885e4b8acf25708839bf17f9f80b9d42
Luna Chouchou <liu2shu1@outlook.com>
2023-10-26 07:12:20 +0900

    src差分確認用_03_src_NavLink高亮

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,7 @@
-import React,{useState} from 'react'
-import {Navigate} from 'react-router-dom' 
+import React from 'react'
 
 export default function Home() {
-  const [sum,setSum] = useState(1)
     return (
-      <div>
         <h3>我是Home的内容</h3>
-        {/* 根据sum的值决定是否切换视图 */}
-        {sum === 2 ? <Navigate to="/about" replace={true}/> : <h4>当前sum的值是：{sum}</h4>}
-        <button onClick={()=>setSum(2)}>点我将sum变为2</button>
-      </div>
     )
 }
