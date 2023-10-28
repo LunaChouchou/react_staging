61d34a21f6c3e2785b0c5a76dd38496c68d9ccc4
Luna Chouchou <liu2shu1@outlook.com>
2023-10-28 22:11:33 +0900

    src差分確認用_08_src_路由的state参数

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import { useLocation, useSearchParams } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 export default function Detail() {
-    //useSearchParams取值
-    const [search,setSearch] = useSearchParams()
-    const id = search.get('id')
-    const title = search.get('title')
-    const content = search.get('content')
-    //useLocation取值
-    const x = useLocation()
-    console.log('@',x)
+    const {state:{id,title,content}} = useLocation()
+
     return (
         <ul>
-        <li>
-            <button onClick={()=>{setSearch('id=008&title=哈哈&content=嘻嘻')}}>点我更新一下收到的search参数</button>
-        </li>
             <li>消息编号：{id}</li>
             <li>消息标题：{title}</li>
             <li>消息内容：{content}</li>
